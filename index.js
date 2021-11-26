4e89b1739c36e7364b8d785186851bb979297ee5
SeanMelfrancoUy <seanmelfrancouy@outlook.com>
2021-11-26 16:56:27 +0000

    First Prep

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const http = require('http'), //This module provides the HTTP server 
-                              //functionalities
-      path = require('path'), //The path module provides utilities for working with file 
-                              //and directory paths
-      express = require('express'), //This module allows this app to respond to HTTP requests, 
-                                    //defines the routing and renders back the required content
-      fs = require('fs'), //This module allows to work with the file system:
-                          //read and write files back
+const http = require('http'), //This module provides the HTTP server functionalities
+      path = require('path'), //The path module provides utilities for working with file and directory paths
+      express = require('express'), //This module allows this app to respond to HTTP requests, defines the routing and renders back the required content
+      fs = require('fs'), //This module allows to work with the file system: read and write files back
       xmlParse = require('xslt-processor').xmlParse, //This module allows to work with XML files
-      xsltProcess = require('xslt-processor').xsltProcess, //This same module allows us to utilise
-                          //XSL Transformations
-      xml2js = require('xml2js') //This module does XML - JSON conversion
+      xsltProcess = require('xslt-processor').xsltProcess, //This same module allows us to utilise XSL Transformations
+      xml2js = require('xml2js'); //This module does XML - JSON conversion
+
+const router = express(),
+      server = http.createServer(router);
+
+router.get('/', function(req, res) {
+
+       res.writeHead(200, {'Content-Type' : 'text/html'});
+
+       let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+           xsl = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+        
+       let doc = xmlParse(xml);
+            stylesheet = xmlParse(xsl);
+
+       let result = xsltProcess(doc, stylesheet);
+
+       res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
